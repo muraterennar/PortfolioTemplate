0f58f93f5ce3b1cd40182f51db0c7595457bd4ad
Murat Eren Nar <muraterennar@hotmail.com>
2022-10-30 20:35:56 +0300

    Add masonry pack

---
+++ Assests/js/custom/script.js
@@ -0,0 +1,21 @@
+/* ======== FILTERS JS ======== */
+
+const tabs = document.querySelectorAll("[data-target]"),
+  tabContents = document.querySelectorAll("[data-content]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    tabContents.forEach((tc) => {
+      tc.classList.remove("filters_active");
+    });
+
+    target.classList.add("filters_active");
+
+    tabs.forEach((t) => {
+      t.classList.remove("filters-tab-active");
+    });
+    tab.classList.add("filters-tab-active");
+  });
+});
