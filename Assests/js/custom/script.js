1fb944ef03d934e0d44816348a94a2a1455eeac7
Murat Eren Nar <muraterennar@hotmail.com>
2022-11-01 12:31:36 +0300

    01.11.202 Commit 1 | Added Scroll Animation

--- Assests/js/custom/script.js
+++ Assests/js/custom/script.js
@@ -1,3 +1,17 @@
+ScrollReveal().reveal(".headline-header", { delay: 200, distance: "-40px" });
+ScrollReveal().reveal(".headline-main-button", {
+  delay: 400,
+  distance: "60px",
+});
+ScrollReveal().reveal(".btn-color", { delay: 500, distance: "-30px" });
+ScrollReveal().reveal(".card", {
+  delay: 500,
+  distance: "30px",
+  origin: "left",
+});
+ScrollReveal({ reset: true });
+// ScrollReveal().reveal('.headline');
+
 /* ======== FILTERS JS ======== */
 
 const tabs = document.querySelectorAll("[data-target]"),
